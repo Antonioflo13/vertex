ac0ef3594c4c0bd0200c7706665ff8ecb08185e0
Antonioflo13 <floreantonio@hotmail.it>
2024-06-15 16:03:08 +0200

    Add art in progress message to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <section className="my-auto">
-        <h1 className="text-3xl">VERTEX</h1>
+      <section className="flex flex-col justify-center items-center my-auto">
+        <h1 className="text-3xl pb-10">VERTEX</h1>
+        <p className="text-sm text-gray-500">art in progress...</p>
       </section>
     </main>
   );
